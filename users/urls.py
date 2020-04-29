2771c83e13d5acf7a0bf40519a4d5fe974ead815
g478361787 <02051007gaolele>
2020-04-29 16:39:43 +0800

    测试子应用

--- users/urls.py
+++ users/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url,include
+from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r"^users/",include("users.urls"))
+    url(r"^index/$",views.index),
 ]
